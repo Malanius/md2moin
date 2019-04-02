0bdcc34e61fc5570ae009584202fd5f613beaa0f
Malanius <malaniusprivierre@gmail.com>
2019-04-02 08:54:18 +0200

    Setup Thymeleaf decoupled logic

---
+++ md2moin-web/src/main/java/net/malanius/md2moin/web/util/thymeleaf/DecoupledLogicSetup.java
@@ -0,0 +1,24 @@
+package net.malanius.md2moin.web.util.thymeleaf;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@Component
+public class DecoupledLogicSetup {
+
+    private final SpringResourceTemplateResolver templateResolver;
+
+    public DecoupledLogicSetup(SpringResourceTemplateResolver templateResolver) {
+        this.templateResolver = templateResolver;
+    }
+
+    @PostConstruct
+    private void init() {
+        templateResolver.setUseDecoupledLogic(true);
+        log.debug("Decoupled template logic enabled.");
+    }
+}
