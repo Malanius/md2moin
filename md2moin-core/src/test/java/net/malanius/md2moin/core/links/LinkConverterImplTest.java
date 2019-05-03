89e4275c105e6eafd885253a39999fed98720593
Malanius <malaniusprivierre@gmail.com>
2019-05-03 21:39:02 +0200

    Create link conversion test

---
+++ md2moin-core/src/test/java/net/malanius/md2moin/core/links/LinkConverterImplTest.java
@@ -0,0 +1,32 @@
+package net.malanius.md2moin.core.links;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LinkConverterImplTest {
+
+    private LinkConverter converter;
+
+    @Before
+    public void setUp() throws Exception {
+        converter = new LinkConverterImpl();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        converter = null;
+    }
+
+    @Test
+    public void convertLink() {
+        String input = "This is some text with [Example](https://example.com) link. And yet [another](www.example.com) one.\n"
+                + "This is [link](http://example.com) on another line.";
+        String expected = "This is some text with [[https://example.com|Example]] link. And yet [[www.example.com|another]] one.\n"
+                + "This is [[http://example.com|link]] on another line.";
+
+        assertEquals(expected, converter.convertLink(input));
+    }
+}
