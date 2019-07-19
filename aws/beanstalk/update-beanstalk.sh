f8b023bb08a93a5d9bb63c8aa6f1af6efed829b7
Malanius Privierre <malaniusprivierre@gmail.com>
2019-07-19 13:31:17 +0200

    Create beanstalk environment for the app

---
+++ aws/beanstalk/update-beanstalk.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+ENV=${1}
+if [[ ${ENV} == "dev" ]] || [[ ${ENV} == "int" ]] || [[ ${ENV} == "ope" ]]; then
+        echo "Creating stack with ${ENV^^} configuration...";
+        . ../_scripts/env.sh ${ENV}
+         aws cloudformation update-stack \
+            --template-body "file://beanstalk.yaml" \
+            --stack-name "${APP_NAME}-${APP_ENV}-beanstalk" \
+            --tags \
+                "Key=Env,Value=${APP_ENV^^}" \
+                "Key=tf:owner,Value=${OWNER}" \
+            --parameters \
+                "ParameterKey=AppName,ParameterValue=${APP_NAME}" \
+                "ParameterKey=AppEnv,ParameterValue=${APP_ENV}" \
+                "ParameterKey=DeployEnv,ParameterValue=${DEPLOY_ENV}" \
+                "ParameterKey=NetworkStack,ParameterValue=vpc-info" \
+            --capabilities CAPABILITY_NAMED_IAM \
+            --profile "${AWS_PROFILE}"
+         if [[ ${?} -eq 0 ]]; then
+            echo "Stack updated."
+         else
+            echo "Something went wrong during stack creation!"
+         fi
+    else
+        echo "Wrong environment specified, not executing update goal!"
+fi
+
+. ../_scripts/reset.sh
